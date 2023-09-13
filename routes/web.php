e1716cdcd0f70f4e588f870591e74f977446590e
Fabian Suarez <fsuarez120@unab.edu.co>
2023-09-12 19:04:37 -0500

    Primer commit clase 12-09-2023

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return "HOME PAGE";
+});
+
+Route::get('productos', function () {
+    return "LISTADO DE TODOS LOA PRODUCTOS";
+});
+
+Route::get('productos/create', function () {
+    return "AQUI VA A ESTAR EL FORMULARIO DE LA CREACION DE PRODUCTOS";
+});
+
+Route::get('productos/{id}', function ($id) {
+    return "el nombre del producto es $id";
+});
+
+Route::get('productos/{id}/{categoria}', function ($id,$categoria) {
+    return "el nombre del producto es $id con la categoria $categoria";
+});
